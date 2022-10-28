4cbec9c58662bee11d571e68c584a77f280661d9
Ainamaani Isaac <aina.isaac2002@gmail.com>
2022-10-28 16:45:32 +0300

    First Commit

---
+++ transportug/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TransportugConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'transportug'
---
+++ transportug/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class BusCompany(models.Model):
+    companyName = models.CharField(max_length=30)
+    region = models.CharField(max_length=30)
+    busImage = models.ImageField(null=True,upload_to='buses')
+    shiftsTo = models.IntegerField()
+    shiftsFrom = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.companyName
+
+class Bus(models.Model):
+    company = models.ForeignKey(BusCompany,on_delete = models.CASCADE)
+    numberPlate = models.CharField(max_length=30)
+    mainOffices = models.CharField(max_length=30)
+    destination = models.ForeignKey(BusCompany,related_name='regions',on_delete=models.DO_NOTHING)
+    travelTime = models.TimeField()
+    seats = models.IntegerField()
+    status = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.company
+
+class Prices(models.Model):
+    startLocation = models.ForeignKey(BusCompany,on_delete=models.DO_NOTHING)
+    destination = models.CharField(max_length=30)
+    price = models.IntegerField()
+    busCompany = models.CharField(max_length=30)
+    setOffTime = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.busCompany
+    
+
+
