dedbac100e8e8a992ba7f287e34da54ef3a7e128
Ainamaani Isaac <aina.isaac2002@gmail.com>
2022-10-28 19:37:15 +0300

    Changed views

--- transportug/views.py
+++ transportug/views.py
@@ -3,7 +3,7 @@ from .models import Bus,BusCompany,Prices
 
 # Create your views here.
 def home(request):
-    companies = BusCompany.objects.all()
+    companies = BusCompany.objects.all().order_by('region')
     return render(request,'home.html',{
         'companies' : companies
     })
